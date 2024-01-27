36aa85518018e698e747be9dfbe536b8ac930640
LukeZyte <lukasz.jarzab01@gmail.com>
2024-01-27 15:27:33 +0100

    init

---
+++ API/Controllers/BaseApiController.cs
@@ -0,0 +1,12 @@
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class BaseApiController : ControllerBase
+    {
+        
+    }
+}
